6db50a7adce2d492a6ad842060cc5ad13259eccf
9one9 <brayson@albany.edu>
2017-10-05 22:48:16 -0400

    More GUI Work,GridLayout

--- src/ContactWindow.java
+++ src/ContactWindow.java
@@ -8,6 +8,15 @@ import java.awt.*;
 public class ContactWindow extends JFrame{
 
     private JPanel panel;
+    private JPanel panel2;
+    private JPanel panel3;
+    private JPanel panel4;
+    private JPanel panel5;
+
+
+
+
+
     private JLabel label;
     private JLabel label2;
     private JLabel label3;
@@ -25,23 +34,38 @@ public class ContactWindow extends JFrame{
 
     public ContactWindow(){
 
-        final int window_WIDTH = 400;
-        final int window_HEIGHT = 700;
+        final int window_WIDTH = 200;
+        final int window_HEIGHT = 400;
 
 
         setTitle("(Testing) Contacts");
         setSize(window_WIDTH,window_HEIGHT);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         buildPanel();
+
         add(panel);
+        add(panel2);
+        add(panel3);
+        add(panel4);
+        add(panel5);
+
+
         pack();
         setVisible(true);
-        setLayout(new GridLayout(4,2));
+        setLayout(new GridLayout(5,2,0,1));
 
     }
 
     //Panes, then panels, then components, like text and buttons.
     private void buildPanel(){
+
+        panel = new JPanel();
+        panel2 = new JPanel();
+        panel3= new JPanel();
+        panel4 = new JPanel();
+        panel5 = new JPanel();
+
+
         label = new JLabel("First Name:");
         label2 = new JLabel("Last Name:");
         label3 = new JLabel("Phone Number:");
@@ -55,31 +79,24 @@ public class ContactWindow extends JFrame{
         button = new JButton("Go");
         button2 = new JButton("Return");
 
-        panel = new JPanel();
-        panel2 = new JPanel();
-        panel3 = new JPanel();
-        panel4 = new JPanel();
-        panel5 = new JPanel();
-        panel6 = new JPanel();
-        panel7 = new JPanel();
-        panel8 = new JPanel();
 
-        /**
+
+
          panel.add(label);
-         panel.add(label2);
-         panel.add(label3);
-         panel.add(label4);
+         panel2.add(label2);
+         panel3.add(label3);
+         panel4.add(label4);
 
          //This panel describes an input window where contacts can be created.
-         **/
 
-        panel.add(text, BorderLayout.CENTER);
-        panel.add(text2, BorderLayout.CENTER);
-        panel.add(text3);
-        panel.add(text4);
 
-        panel.add(button);
-        panel.add(button2);
+        panel.add(text);
+        panel2.add(text2);
+        panel3.add(text3);
+        panel4.add(text4);
+
+        panel5.add(button);
+        panel5.add(button2);
 
 
 
